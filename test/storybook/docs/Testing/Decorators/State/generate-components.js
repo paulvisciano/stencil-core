898b1b92e42266e8a1ab9007b1212fbc6e85ddf4
paulvisciano <paulvisciano.dev@gmail.com>
2025-10-09 22:52:35 -0400

    Fix the path

--- test/storybook/docs/Testing/Decorators/State/generate-components.js
+++ test/storybook/docs/Testing/Decorators/State/generate-components.js
@@ -7,7 +7,7 @@ const __dirname = path.dirname(__filename);
 
 const DATA_PATH = path.resolve(__dirname, 'coverage-data.json');
 const RULES_PATH = path.resolve(__dirname, 'rules.json');
-const OUTPUT_DIR = path.resolve(__dirname, '../../../../../test/wdio/state/matrix');
+const OUTPUT_DIR = path.resolve(__dirname, '../../../../../wdio/state/matrix');
 
 function toPascalCase(tag) {
   return tag
