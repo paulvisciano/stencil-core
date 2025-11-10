8dba9eccdf362c07d45e5d8da4ac390bea1be37a
paulvisciano <paulvisciano.dev@gmail.com>
2025-11-10 17:02:26 +0800

    Run all the ts-target by default

--- test/wdio/wdio.conf.ts
+++ test/wdio/wdio.conf.ts
@@ -67,7 +67,7 @@ export const config: WebdriverIO.Config = {
   // The path of the spec files will be resolved relative from the directory of
   // of the config file unless it's absolute.
   //
-  specs: [['./extends/extends.test.tsx']],
+  specs: [['./ts-target/**/*.test.ts']],
   // Patterns to exclude.
   exclude: ['./node_modules/**'],
   //
